cccdc8f19da29a9c7b7e7f609247f1863f26193a
Ekleccia Reydianto <93103544+EklecciaR27@users.noreply.github.com>
2022-10-05 07:42:16 +0800

    comment

--- PostTest3/javascript.js
+++ PostTest3/javascript.js
@@ -5,6 +5,6 @@ function ubahWarna(){
     element.classList.toggle("light");
 }
 
-
+// Maaf Bang dan Mba, untuk penggunaan Eventlistener dan DOM scriptnya terdapat di index.html, dikarenakan kemarin script yang disini hanya bisa terbaca bagian function
 
 
