3bc0badf34d448eaa577f38cf29ba7324d554d82
Ekleccia Reydianto <93103544+EklecciaR27@users.noreply.github.com>
2022-10-29 22:57:09 +0800

    Add files via upload

---
+++ database_pv.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 29, 2022 at 04:54 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database_pv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `form_pv`
+--
+
+CREATE TABLE `form_pv` (
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` int(12) NOT NULL,
+  `dob` date NOT NULL,
+  `message` varchar(1000) NOT NULL,
+  `submit` text NOT NULL,
+  `id` int(11) NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `form_pv`
+--
+
+INSERT INTO `form_pv` (`nama`, `email`, `phone`, `dob`, `message`, `submit`, `id`, `gambar`) VALUES
+('Selena Gomez', 'SG@gmail.com', 2147483647, '2022-10-10', 'AYOKKKK', '12:32:22pm', 3, ''),
+('Lalisa Manobal', 'swift@gmail.com', 2147483647, '2022-10-14', 'AYUUUUkkkkkk', '06:09:43am', 4, ''),
+('Ekleccia R', 'ekleccia.reydianto88@gmail.com', 928173121, '2022-10-06', 'Ya begitu saja', '11:29:02am', 8, ''),
+('Rannel Elgio', 'elgio@gmail.com', 1234321, '2022-10-14', 'yyyyyy', '11:58:48am', 9, ''),
+('ciaaakk', 'cia21@gmail.com', 1212321212, '0000-00-00', '', '09:52:43pm', 10, 'ciaaakk.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `form_pv`
+--
+ALTER TABLE `form_pv`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `EMAIL` (`email`,`phone`,`dob`),
+  ADD UNIQUE KEY `nama` (`nama`),
+  ADD UNIQUE KEY `Message` (`message`,`submit`) USING HASH;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `form_pv`
+--
+ALTER TABLE `form_pv`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
